a09b799b976896b51229668be19f49e7df461b96
Vítor <vitortls93@gmail.com>
2022-02-23 22:13:44 -0300

    Mudanças na estrutura do site, mudanças no css

--- config.php
+++ config.php
@@ -1,6 +1,5 @@
 <?php
 $pdo = new PDO('mysql:dbname=darkprojectdb;host=localhost','root','rebelsA1*');
 $sql = $pdo->query('SELECT * FROM usuarios');
-$dados = $sql->fetchAll();
-print_r($dados);
+
 ?>
